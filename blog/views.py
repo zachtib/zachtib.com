364eff9aec001f174bd0eafc94264dedf4c28f3e
Zach Tibbitts <zachtib@gmail.com>
2011-03-01 14:56:49 -0500

    Add a simple view to list posts

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,14 @@
+from django.template import Context, loader
+from blog.models import Post
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Hello, World')
+    latest_post_list = Post.objects.all().order_by('-postdate')[:5]
+    t = loader.get_template('blog/index.html')
+    c = Context({
+        'latest_post_list': latest_post_list,
+    })
+    return HttpResponse(t.render(c))
 
 def post(request, post_id):
     return HttpResponse('Viewing post %s' % post_id)
