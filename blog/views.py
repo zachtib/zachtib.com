7f85355bba3937e293cbb563e3897fc607f7f1b0
Zach Tibbitts <zachtib@gmail.com>
2011-03-01 16:12:11 -0500

    View for posts

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,10 @@
-from django.template import Context, loader
+from django.shortcuts import render_to_response, get_object_or_404
 from blog.models import Post
-from django.http import HttpResponse
 
 def index(request):
     latest_post_list = Post.objects.all().order_by('-postdate')[:5]
-    t = loader.get_template('blog/index.html')
-    c = Context({
-        'latest_post_list': latest_post_list,
-    })
-    return HttpResponse(t.render(c))
+    return render_to_response('blog/index.html', {'latest_post_list': latest_post_list})
 
 def post(request, post_id):
-    return HttpResponse('Viewing post %s' % post_id)
+    p = get_object_or_404(Post, pk=post_id)
+    return render_to_response('blog/post.html', {'post': p})
