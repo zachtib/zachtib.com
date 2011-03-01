7fa40de666f664db5c2b65837f3332d442566a73
Zach Tibbitts <zachtib@gmail.com>
2011-03-01 14:03:13 -0500

    Add inital model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(User)
+    title = models.CharField(max_length=256)
+    subtitle = models.CharField(max_length=256)
+    postdate = models.DateTimeField('Post Date')
+    editdate = models.DateTimeField('Edit Date')
+    text = models.TextField('Post Text')
