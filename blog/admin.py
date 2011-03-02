a7a7baf25fd08d09b41cb2515d8e32e9c4b26930
Zach Tibbitts <zachtib@gmail.com>
2011-03-01 19:16:03 -0500

    default to the current user on new Posts

--- blog/admin.py
+++ blog/admin.py
@@ -4,6 +4,19 @@ from django.contrib import admin
 class PostAdmin(admin.ModelAdmin):
     fields = ['title', 'subtitle', 'author', 'text']
 
+    def get_form(self, req, obj=None, **kwargs):
+        self.current_user = req.user
+        return super(PostAdmin, self).get_form(req, obj, **kwargs)
+
+    def formfield_for_dbfield(self, field, **kwargs):
+        from django import forms
+        from django.contrib.auth import models
+        if field.name == 'author':
+            queryset = models.User.objects.all()
+            return forms.ModelChoiceField(
+                queryset=queryset, initial=self.current_user.id)
+        return super(PostAdmin, self).formfield_for_dbfield(field, **kwargs)
+
 admin.site.register(Post, PostAdmin)
 
 class CommentAdmin(admin.ModelAdmin):
