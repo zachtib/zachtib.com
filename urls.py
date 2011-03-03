cd0a46d08fe91a65d7d19e0373536f8f9c7f514a
Zach Tibbitts <zachtib@gmail.com>
2011-03-02 22:40:54 -0500

    Initial styles

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -12,6 +13,9 @@ urlpatterns = patterns('',
     (r'^blog/post/(?P<post_id>\d+)/$', 'blog.views.post'),
     (r'^blog/post/(?P<post_id>\d+)/comment/$', 'blog.views.comment'),
     (r'^blog/tag/(?P<tag_id>\d+)/$', 'blog.views.tag'),
+
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT}),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
