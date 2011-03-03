5ea92aa5ba73d84e4b17d7edc810eb61ff114436
Zach Tibbitts <zachtib@gmail.com>
2011-03-03 16:31:09 -0500

    Enable markup formatting for pages, posts, and comments

--- settings.py
+++ settings.py
@@ -89,6 +89,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'django.contrib.markup',
     'django.contrib.messages',
     'django.contrib.admin',
     'django.contrib.admindocs',
