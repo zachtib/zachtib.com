c55b27aac58f4f5efa1ccc9890b8844e5de8a685
Zach Tibbitts <zachtib@gmail.com>
2011-03-04 18:57:00 -0500

    add first widget

--- widgets/views.py
+++ widgets/views.py
@@ -1,8 +1,16 @@
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
+
+from blog.models import Tag
+
+def tags():
+    tags = Tag.objects.all()
+    for tag in tags:
+        tag.count = len(tag.post_set.all())
+    return render_to_response('widgets/tags.html', {'tags': tags})
 
 def load(request, widget):
     try:
-        return locals()[widget]()
+        return globals()[widget]()
     except KeyError:
         return HttpResponse('Could not locate widget: %s' % widget)
-
