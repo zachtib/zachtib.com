5fbaa151ec6094656ff28513db3f33ea1d7b76ec
Zach Tibbitts <zachtib@gmail.com>
2011-03-22 15:01:13 -0400

    Return a 404 page if a static page cannot be found rather than a Django error

--- pages/views.py
+++ pages/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render_to_response, get_object_or_404
 from pages.models import Page
 
 def render(request, page_name):
-    page = Page.objects.get(title__iexact=page_name)
+    page = get_object_or_404(Page, title__iexact=page_name)
     return render_to_response('pages/page.html', {'page': page,
                                                     'name': page_name})
