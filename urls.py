6c35c9066920802eb006ee1fd3a2f72193b19476
Zach Tibbitts <zachtib@gmail.com>
2011-03-23 14:51:29 -0400

    Undo last commit, it seems to break on apache

--- urls.py
+++ urls.py
@@ -19,7 +19,6 @@ urlpatterns = patterns('',
     (r'^blog/$', 'blog.views.index'),
     (r'^blog/feed/', LatestPostsFeed()),
     (r'^blog/(?P<post_id>\d+)/(?P<post_slug>[-\w]+)/$', 'blog.views.post'),
-    (r'^blog/(?P<post_id>\d+)/$', 'blog.views.post'),
     (r'^blog/comment/(?P<post_id>\d+)/$', 'blog.views.comment'),
     (r'^blog/tag/(?P<tag_name>\w+)/$', 'blog.views.tag'),
 
