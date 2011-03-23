d788e620565889855a5c577bd6cad49d6ee8a24c
Zach Tibbitts <zachtib@gmail.com>
2011-03-23 10:57:29 -0400

    Allow pages to be blank

--- pages/models.py
+++ pages/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Page(models.Model):
     title = models.CharField(max_length=256)
-    text = models.TextField('Page Text')
+    text = models.TextField('Page Text', blank=True)
 
     def __unicode__(self):
         return self.title
