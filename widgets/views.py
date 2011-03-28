018073c1c4edc9fe0e1ae76779943f2d635a82c0
Zach Tibbitts <zachtib@gmail.com>
2011-03-28 13:27:43 -0400

    basic Twitter in sidebar

--- widgets/views.py
+++ widgets/views.py
@@ -3,11 +3,23 @@ from django.shortcuts import render_to_response
 
 from blog.models import Tag
 
+import urllib
+import simplejson
+
 def tags():
     tags = Tag.objects.all()
     for tag in tags:
         tag.count = len(tag.post_set.all())
     return render_to_response('widgets/tags.html', {'tags': tags})
+
+def twitter():
+    USERNAME = 'zachtib'
+    s = urllib.urlopen('http://twitter.com/status/user_timeline/%s.json' \
+        '?count=10' % USERNAME).read()
+    o = simplejson.loads(s)
+    return render_to_response('widgets/twitter.html', {'tweets': o})
+
+
 
 def load(request, widget):
     try:
